966f4b221b4366d636b593522b89e9ea03c760fd
keys8410 <yanalmeidagarcia@gmail.com>
2020-10-06 18:13:53 -0300

    Api docs v1

---
+++ public/docs/api_project.js
@@ -0,0 +1,15 @@
+define({
+  "name": "Support Center",
+  "version": "0.0.2",
+  "description": "API Node/Express",
+  "title": "Support Center API Docs",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2020-10-06T21:11:22.982Z",
+    "url": "https://apidocjs.com",
+    "version": "0.25.0"
+  }
+});
