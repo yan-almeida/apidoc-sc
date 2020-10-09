e8fc3cb9e91f58fbfad2bb65af375bea852127a2
keys8410 <yanalmeidagarcia@gmail.com>
2020-10-09 00:08:45 -0300

    Metodo POST adicionado + correções nas requisições GET, ambos na rota de usuários

--- docs/api_project.js
+++ docs/api_project.js
@@ -8,7 +8,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-10-06T21:11:22.982Z",
+    "time": "2020-10-09T03:05:31.240Z",
     "url": "https://apidocjs.com",
     "version": "0.25.0"
   }
