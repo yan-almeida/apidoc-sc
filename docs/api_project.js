eae6a0a1e1229547391c40f838c3b944bf26c957
keys8410 <yanalmeidagarcia@gmail.com>
2020-10-23 19:09:46 -0300

    DELETE method + change colors UI

--- docs/api_project.js
+++ docs/api_project.js
@@ -8,7 +8,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-10-23T20:07:20.321Z",
+    "time": "2020-10-23T22:07:20.272Z",
     "url": "https://apidocjs.com",
     "version": "0.25.0"
   }
