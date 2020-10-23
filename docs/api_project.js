e03f827c9a144a0eb22c2b7a1bb6e4adefda7a7b
keys8410 <yanalmeidagarcia@gmail.com>
2020-10-23 12:05:43 -0300

    POST method

--- docs/api_project.js
+++ docs/api_project.js
@@ -8,7 +8,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-10-22T19:08:11.893Z",
+    "time": "2020-10-22T20:57:42.830Z",
     "url": "https://apidocjs.com",
     "version": "0.25.0"
   }
