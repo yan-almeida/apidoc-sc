01509fe7c13ff21874d714f6ebc7852e62f678dc
keys8410 <yanalmeidagarcia@gmail.com>
2020-10-23 17:07:38 -0300

    Reorganizando o padrão de resposta

--- docs/api_project.js
+++ docs/api_project.js
@@ -8,7 +8,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-10-23T19:17:53.487Z",
+    "time": "2020-10-23T20:07:20.321Z",
     "url": "https://apidocjs.com",
     "version": "0.25.0"
   }
