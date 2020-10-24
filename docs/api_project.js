48a74bad3ad6152fa44a4310d03eba6964999d29
keys8410 <yanalmeidagarcia@gmail.com>
2020-10-24 15:49:11 -0300

    correçoes na rota de auth login

--- docs/api_project.js
+++ docs/api_project.js
@@ -8,7 +8,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-10-24T18:43:55.638Z",
+    "time": "2020-10-24T18:48:48.419Z",
     "url": "https://apidocjs.com",
     "version": "0.25.0"
   }
