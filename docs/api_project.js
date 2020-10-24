2afa8be43b9feaaa2e2a100d4d9888bfd0236200
keys8410 <yanalmeidagarcia@gmail.com>
2020-10-24 15:44:20 -0300

    Rota de auth user documentada

--- docs/api_project.js
+++ docs/api_project.js
@@ -8,7 +8,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-10-23T22:07:20.272Z",
+    "time": "2020-10-24T18:43:55.638Z",
     "url": "https://apidocjs.com",
     "version": "0.25.0"
   }
