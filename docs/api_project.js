59f821d421c9b416f22cd273485973809a6dc309
keys8410 <yanalmeidagarcia@gmail.com>
2020-10-26 14:24:22 -0300

    @apiUse reorganizado + doc. da rota de auth/forgot-password criada

--- docs/api_project.js
+++ docs/api_project.js
@@ -8,7 +8,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-10-24T18:48:48.419Z",
+    "time": "2020-10-26T17:22:35.757Z",
     "url": "https://apidocjs.com",
     "version": "0.25.0"
   }
